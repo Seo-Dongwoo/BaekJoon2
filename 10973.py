945f025b306a907ad9ae103cfc23b646d8b1fd26
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-06-01 19:31:44 +0900

    10973-이전순열

---
+++ 10973.py
@@ -0,0 +1,13 @@
+import sys
+N = int(sys.stdin.readline())
+L = list(map(int, sys.stdin.readline().split()))
+
+for i in range(N-1, 0, -1):
+    if L[i-1] > L[i]:
+        for j in range(N-1, 0, -1):
+            if L[i-1] > L[j]:
+                L[i-1], L[j] = L[j], L[i-1]
+                L = L[:i] + sorted(L[i:], reverse=True)
+                print(*L)
+                exit()
+print(-1)
