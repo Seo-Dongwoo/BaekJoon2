832a74585b1f1f813ef0546c9e0c18d05798c76d
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-06-01 19:44:47 +0900

    10974-모든순열

---
+++ 10974.py
@@ -0,0 +1,15 @@
+import sys
+N = int(sys.stdin.readline())
+s = []
+
+def dfs():
+    if len(s) == N:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, N+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+dfs()
