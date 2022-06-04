91fd46175a9b748a79a04da41461e1c10198ec74
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-06-04 18:36:49 +0900

    9095-123더하기

---
+++ 9095.py
@@ -0,0 +1,12 @@
+import sys
+T = int(sys.stdin.readline())
+cache = [0] * 11
+cache[1] = 1
+cache[2] = 2
+cache[3] = 4
+
+for i in range(4, 11):
+    cache[i] = sum(cache[i-3:i])
+
+for _ in range(T):
+    print(cache[int(sys.stdin.readline())])
