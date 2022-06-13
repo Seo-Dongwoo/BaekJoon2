b1c1f5b1bd9223b70d23c2556a72b5ab6591f151
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-06-13 13:08:35 +0900

    1931-회의실 배정

---
+++ 1931.py
@@ -0,0 +1,16 @@
+N = int(input())
+s = []
+for i in range(N):
+    first, second = map(int, input().split())
+    s.append([first, second])
+
+s = sorted(s, key= lambda a : a[0])
+s = sorted(s, key= lambda a : a[0])
+last_end = 0
+cnt = 0
+
+for first, second in s:
+    if first > last_end:
+        cnt += 1
+        last_end = second
+print(cnt)
